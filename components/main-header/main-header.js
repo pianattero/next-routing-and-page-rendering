ad8bf62242e3797222eb3506021baf69968c0e16
María Pía Nattero <mariapianattero@gmail.com>
2024-09-24 15:23:06 +0200

    REFACTOR: style main header and set it on all pages

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,9 +1,18 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function MainHeader() {
-    return (
-        <header>
-            <Link href='/news'>News</Link>
-        </header>
-    )
+  return (
+    <header id="main-header">
+      <div id="logo">
+        <Link href="/">NextNews</Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/news">News</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
